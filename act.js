55e24e0f20398bf7bb4392c1ea0c0d77c7974519
Behruzbek Rajabov <behrouz.rajabov911574210@gmail.com>
2025-10-27 17:47:23 +0500

    Yangi telegram abot api

--- act.js
+++ act.js
@@ -1,9 +1,11 @@
 import TelegramBot from "node-telegram-bot-api";
 const TOKEN = "8474325094:AAG764eJWvrXDdHrarCZyLPGP8qASWeV5EE";
-const bot = new TelegramBot(TOKEN, {polling:true});
+const bot = new TelegramBot(TOKEN, { polling: true });
 bot.on("message", function (msg) {
-    
-    const chatId = msg.chat.id;
+  const chatId = msg.chat.id;
 
-    bot.sendMessage(chatId, "Salom");
+  // bot.sendMessage(chatId, "Salom");
+  if (msg.text == "/start") {
+    bot.sendMessage(chatId, ` Behruzbek ${bot.sendMessage(chatId, "Salom")}`);
+  }
 });
