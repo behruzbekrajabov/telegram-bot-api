3dc96d820a7b77fa40c741671d3a8d12f8ec739b
Behruzbek Rajabov <behrouz.rajabov911574210@gmail.com>
2025-10-27 18:16:14 +0500

    Button qoshildi

--- act.js
+++ act.js
@@ -3,9 +3,26 @@ const TOKEN = "8474325094:AAG764eJWvrXDdHrarCZyLPGP8qASWeV5EE";
 const bot = new TelegramBot(TOKEN, { polling: true });
 bot.on("message", function (msg) {
   const chatId = msg.chat.id;
+  const text = msg.text;
+  const firsname = msg.chat.first_name;
 
-  // bot.sendMessage(chatId, "Salom");
   if (msg.text == "/start") {
-    bot.sendMessage(chatId, ` Behruzbek ${bot.sendMessage(chatId, "Salom")}`);
+    bot.sendMessage(chatId, `Xush kelibsiz, ${firsname}`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash" }][{ text: "Sozlamalar" }][{ text: "Menu" }],
+        ],
+        resize_keyboard: true,
+      },
+    });
+  }else if(text == "Boshlash")
+  {
+    bot.sendMessage(chatId, "bOSHLANDI");
+  }else if(text == "Menu")
+  {
+    bot.sendMessage(chatId, "mENU");
+  }else if(text == "Sozlamalar")
+  {
+    bot.sendMessage(chatId, "Sozlamalar");
   }
 });
